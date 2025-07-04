913dd451bda3f5ba6dbcaf02d82284f9c4425e8c
listenfengyang <56905318+listenfengyang@users.noreply.github.com>
2025-07-04 14:25:12 +0400

    Update header.go
    fix key

--- header.go
+++ header.go
@@ -3,6 +3,6 @@ package go_coinpay
 func getHeaders(hmac string) map[string]string {
 	return map[string]string{
 		"Content-Type": "application/x-www-form-urlencoded",
-		"HTTP_HMAC":         hmac,
+		"HMAC":         hmac,
 	}
 }
